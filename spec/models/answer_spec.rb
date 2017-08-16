35f701baa2cbd44ebd7392175ddf3f59c7275e60
Brian Moore <bjm8498@gmail.com>
2017-08-15 20:06:45 -0700

    Checkpoint 14 answer_spec.rb corrected

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
     let(:question) {Question.create!(title: "New Question Title", body: "New Question body", resolved: true) }
-    let(:answer) {Answer.create!(body: "Answer body") }
+    let(:answer) {Answer.create!(body: "Answer body", question: question) }
     
     describe "attributes" do
         it "has a body attribute" do
