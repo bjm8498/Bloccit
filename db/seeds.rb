91fd8cd0a8c1c3e135cd40c9c9dc6b32e981b369
Brian Moore <bjm8498@gmail.com>
2017-09-16 23:37:04 -0700

    Checkpoint 27 completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -68,10 +68,18 @@ Comment.find_or_create_by(
     body: "I have been made."
     )
 
-user = User.first
-user.update_attributes!(
-  email: 'bjm8498@gmail.com',
-  password: 'helloworld'
+admin = User.create!(
+  name: 'Admin User',
+  email: 'admin@example.com',
+  password: 'helloworld',
+  role: 'admin'
+)
+
+member = User.create!(
+  name: 'Member User',
+  email: 'member@example.com',
+  password: 'helloworld',
+
 )
 
 puts "Seed finished"
