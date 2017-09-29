7f69fc73dea55cedc29906fd89db885b4729588e
Brian Moore <bjm8498@gmail.com>
2017-09-28 21:22:07 -0700

    Checkpoint 31 completed.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,7 +34,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @posts = @users.posts.visible_to(current_user)
+    @posts = @user.posts.visible_to(current_user)
   end
 
 end
